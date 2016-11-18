559fca54877ed23fc330e3340aef63e10b1950c9
SebArola <sebastien.arola@laposte.net>
2016-11-18 07:11:01 +0100

    auto commit by sebastien

--- git.sh
+++ git.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 echo "Modifications éffectuées:"
-git diff
-if [ "$# -eq 0" ];then
-	git add *
+dif=$(git diff)
+if [ "$dif" = "" ];then
+	echo "Aucune modification"
 else
-	git add $*
+	if [ "$# -eq 0" ];then
+		git add *
+	else
+		git add $*
+	fi
+	msg="auto commit by "$USER
+	echo "Message ? (y/n):"
+	read choix
+	if [ "$choix" = "y" ];then
+		echo "Entrer message :"
+		read msg
+	fi
+	git pull
+	git commit -m  "$msg"
+	git push
 fi
-msg="auto commit by "$USER
-echo "Message ? (y/n):"
-read choix
-if [ "$choix" = "y" ];then
-	echo "Entrer message :"
-	read msg
-fi
-git pull
-git commit -m  "$msg"
-git push

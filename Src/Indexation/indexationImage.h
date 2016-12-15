18550b2060717c402e32e7ecfd058a6fff9fb431
SebArola <sebastien.arola@laposte.net>
2016-12-15 12:56:07 +0100

    indexationImage

--- Src/Indexation/indexationImage.h
+++ Src/Indexation/indexationImage.h
@@ -1,5 +1,8 @@
 #ifndef _INDEXATIONIMAGE_H
 #define  _INDEXATIONIMAGE_H
+#include "Fichier.h"
+#include "pileDescripteur.h"
 
-void genDescripteurImage(t_Fichier fichier, pileDescripteur *ptrPileImage);
+void genDescripteurImage(t_Fichier fichier, t_PileDescripteur *ptrPileImage);
+
 #endif

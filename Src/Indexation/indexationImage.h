ad1d6aa14689e493703c2cb18c1f4723758b3021
SebArola <sebastien.arola@laposte.net>
2016-12-26 18:03:55 +0100

    fichier ibdexation

--- Src/Indexation/indexationImage.h
+++ Src/Indexation/indexationImage.h
@@ -3,6 +3,10 @@
 #include "Fichier.h"
 #include "pileDescripteur.h"
 
+/*
+Méthode prenant un Fichier ainsi que la pile de descripteur image. Cette méthode
+crée le descripteur du fichier dans un string et l'enregistre dans la pile.
+*/
 void genDescripteurImage(t_Fichier fichier, t_PileDescripteur *ptrPileImage);
 //MODIF
 #endif
---
+++ Src/Indexation/indexationTexte.h
@@ -0,0 +1,11 @@
+#ifndef _INDEXATIONTEXTE_H
+#define  _INDEXATIONTEXTE_H
+#include "Fichier.h"
+#include "pileDescripteur.h"
+/*
+Méthode prenant un Fichier ainsi que la pile de descripteur texte. Cette méthode
+crée le descripteur du fichier dans un string et l'enregistre dans la pile.
+*/
+void genDescripteurTexte(t_Fichier fichier, t_PileDescripteur *ptrPileImage);
+//MODIF
+#endif

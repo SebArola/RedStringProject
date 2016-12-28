458340c6811d5c5383dcb3a897076a63d2ea4597
EtienneDelarue <etienne.delarue31@gmail.com>
2016-12-28 16:27:40 +0100

    test

--- Src/Indexation/indexationImage.h
+++ Src/Indexation/indexationImage.h
@@ -10,3 +10,4 @@ crée le descripteur du fichier dans un string et l'enregistre dans la pile.
 void genDescripteurImage(t_Fichier fichier, t_PileDescripteur *ptrPileImage);
 //MODIF
 #endif
+testtest

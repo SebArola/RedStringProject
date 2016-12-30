e5352c2bd18bbd213ce36332ea803905082086d6
SebArola <sebastien.arola@laposte.net>
2016-12-30 16:08:08 +0100

    comparaison

--- Src/Comparaison/comparaison.c
+++ Src/Comparaison/comparaison.c
@@ -1,3 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "comparaison.h"
+
+int comparaisonImage(t_Fichier fichierCompare, t_PileDescripteur pileImage){
+    //TODO
+    return 0;
+}
+
+int comparaisonSon(t_Fichier fichierCompare, t_PileDescripteur pileSon){
+    //TODO
+    return 0;
+}
+
+int comparaisonTexte(t_Fichier fichierCompare, t_PileDescripteur pileTexte){
+    //TODO
+    return 0;
+}
+
+int compareFichierImage(t_Fichier fichierDonne , t_Fichier fichierCompare){
+    //TODO
+    return 0;
+}
+
+int compareFichierSon(t_Fichier fichierDonne , t_Fichier fichierCompare){
+    //TODO
+    return 0;
+}
+int compareFichierTexte(t_Fichier fichierDonne , t_Fichier fichierCompare){
+    //TODO
+    return 0;
+}

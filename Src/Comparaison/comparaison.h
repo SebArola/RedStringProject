2c0a0cda80e6bd17e9b117638ada0659d2391fd0
SebArola <sebastien.arola@laposte.net>
2016-12-30 15:42:59 +0100

    comparaison.h fini

--- Src/Comparaison/comparaison.h
+++ Src/Comparaison/comparaison.h
@@ -4,6 +4,10 @@
 #include "../Indexation/Fichier.h"
 #include "../Indexation/pileDescripteur.h"
 
-int comparaisonImage(t_Fichier fichierCompare, t_PileDescripteur ptrPileImage);
+int comparaisonImage(t_Fichier fichierCompare, t_PileDescripteur pileImage);
+
+int comparaisonSon(t_Fichier fichierCompare, t_PileDescripteur pileSon);
+
+int comparaisonTexte(t_Fichier fichierCompare, t_PileDescripteur pileTexte);
 
 #endif

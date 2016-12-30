fe0d9407763fa2c90301f5060a425d7c7734f9c4
SebArola <sebastien.arola@laposte.net>
2016-12-30 14:38:09 +0100

    test

--- Test/test_pileDescripteur.c
+++ Test/test_pileDescripteur.c
@@ -13,3 +13,16 @@ void test_teardown() {
 MU_TEST(test_check){
     mu_check(pile_est_vide(pile));
 }
+
+
+MU_TEST_SUITE(test_suite) {
+	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+
+	MU_RUN_TEST(test_check);
+}
+
+int main(int argc, char *argv[]) {
+	MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+	return 0;
+}

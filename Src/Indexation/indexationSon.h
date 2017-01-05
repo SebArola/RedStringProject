d55c0137f2d071a553912b4b444fddaaa6f33b3f
Tara LEMAIRE <mat0lemaire@gmail.com>
2017-01-05 07:55:29 +0100

    Update indexationSon.h

--- Src/Indexation/indexationSon.h
+++ Src/Indexation/indexationSon.h
@@ -7,6 +7,6 @@
 Méthode prenant un Fichier ainsi que la pile de descripteur son. Cette méthode
 crée le descripteur du fichier dans un string et l'enregistre dans la pile.
 */
-void genDescripteurSon(t_Fichier fichier, t_PileDescripteur *ptrPileImage);
+void genDescripteurSon(t_Fichier fichier, t_PileDescripteur *ptrPileSon);
 //MODIF
 #endif

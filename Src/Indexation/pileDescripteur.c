2169920f32bcd0dda94875616ee23bdd0a637d07
SebArola <sebastien.arola@laposte.net>
2017-01-05 09:48:58 +0100

    rework pileDescripteur (testé et approuvé)

--- Src/Indexation/pileDescripteur.c
+++ Src/Indexation/pileDescripteur.c
@@ -31,28 +31,16 @@ void affiche_pile(t_PileDescripteur pileDescripteur){
 void empile(t_PileDescripteur *p_pileDescripteur, char * descripteur){
 	t_CellDescripteur *nouvCell= malloc(sizeof(t_CellDescripteur));
 	nouvCell->descripteur = malloc(sizeof(char)*strlen(descripteur));
-	nouvCell->p_suivant=NULL;
-	if(p_pileDescripteur->premier!=NULL){
-		t_CellDescripteur *suivant;
-		suivant = p_pileDescripteur->premier;
-		while(suivant->p_suivant!=NULL){
-			suivant = suivant->p_suivant;
-		}
-		suivant->p_suivant = nouvCell;
-	}else{
-		p_pileDescripteur->premier=nouvCell;
-	}
+	nouvCell->p_suivant=p_pileDescripteur->premier;
+	nouvCell->descripteur= descripteur;
+	p_pileDescripteur->premier = nouvCell;
+
 }
 
 int depile(t_PileDescripteur *p_pileDescripteur,char *descripteurDepile){
 	if(!pile_est_vide(*p_pileDescripteur)){
-		t_CellDescripteur *suivant;
-		suivant = p_pileDescripteur->premier;
-		while(suivant->p_suivant->p_suivant!=NULL){
-			suivant = suivant->p_suivant;
-		}
-		descripteurDepile=suivant->p_suivant->descripteur;
-		suivant->p_suivant=NULL;
+		descripteurDepile=p_pileDescripteur->premier->descripteur;
+		p_pileDescripteur->premier = p_pileDescripteur->premier->p_suivant;
 		return 1;
 	}else{
 		return 0;

febf7d9871ae434a0ffcc5162e2b4b336a0316b0
Tara LEMAIRE <mat0lemaire@gmail.com>
2017-01-05 08:30:16 +0100

    Update indexationSon.c

--- Src/Indexation/indexationSon.c
+++ Src/Indexation/indexationSon.c
@@ -12,13 +12,14 @@ void genDecripteurSon(t_Fichier fichier, t_PileDescripteur *ptr_PileSon){
   const int m = 30; //nombre d'intervalles entre -1 et 1
   descripteur = malloc(nb_lignes*sizeof(int));
   ptr_fichSon = fopen(fichier.chemin,"r");
-  float tab[];
+  float tab[n];
+  
 }
 
 int main(int argc, char *argv[]){
   t_PileDescripteur pile;
   t_Fichier fichier;
-  fichier.chemin=malloc(50*sizeof(char));
+  fichier.chemin = malloc(50*sizeof(char));
   fichier.chemin="/Test/fichiers_tests_son/jingle_m6.txt";
   init_pile(&pile);
   genDescripteurSon(fichier, &pile);

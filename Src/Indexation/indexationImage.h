b4c63bee1d45e04cfd3e4cd812a0c9b8be87d471
Etienne Delarue <etienne.delarue31@gmail.com>
2017-01-06 14:55:53 +0100

    maj

--- Src/Indexation/indexationImage.h
+++ Src/Indexation/indexationImage.h
@@ -9,5 +9,7 @@ Méthode prenant un Fichier ainsi que la pile de descripteur image. Cette métho
 crée le descripteur du fichier dans un string et l'enregistre dans la pile.
 */
 void genDescripteurImage(t_Fichier fichier, t_PileDescripteur *ptrPileImage);
+void decimalToBinaire(int x, int *res, int bit);
+int binaireToDecimal(int *bin, int bit);
 //MODIF
 #endif

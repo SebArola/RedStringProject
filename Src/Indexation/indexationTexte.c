484e771cc2ff5410105c294f85dc7a4a6c8520af
AurelienLG <AurelienLG@github.com>
2017-01-08 16:34:04 +0100

    lll

--- Src/Indexation/indexationTexte.c
+++ Src/Indexation/indexationTexte.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "indexationSon.h"
-
-// TESTE
-// JE FAIS DES TETEFTEFETFETEFTEFTEFTF
+#define TAILLE_MAX 2000   //taille des chaine content les mots,
+#define TAILLE_BALISE 100 // Taille des chaine comptenant les balises
+#define TAILLE_MOT 26   // Taille max des mot lu
+#define LONGEUR_MOT_MIN 3 // Longeur minium des mot selectionné dnas le texte. (nb-1)
+#define TAILLE_CHAINE 10000 //Longeur des lignes max.(et du descipteur)
+#define NB_MOT_IMPORTANT_CHOISI 5 // Nombre de mot gardé dans le top recurrence.

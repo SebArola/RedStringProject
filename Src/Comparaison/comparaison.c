f0c3cc4d5a16993740af7c76971916a4c5745321
Etienne Delarue <etienne.delarue31@gmail.com>
2017-01-18 23:18:11 +0100

    ça marche !

--- Src/Comparaison/comparaison.c
+++ Src/Comparaison/comparaison.c
@@ -193,22 +193,22 @@ int compareFichierSon(char * fichierDonne , char * fichierCompare)
 int compareFichierTexte(char * fichierDonne , char * fichierCompare)
 {
   FILE * ptr_ficDonne;
-  char mot[100]=rien;
-  int nbCommun=0, nbMots=0;
+  char mot[100]="";
+  int nbCommun=0, nbMots=0, pourcentage, cestUnMot=0;
   ptr_ficDonne=fopen("tempDonne", "w");   //création d'un fichier contenant le premier desc
   fprintf(ptr_ficDonne, "%s", fichierDonne);    //on écrit le 1er desc dans le fichier
   fclose(ptr_ficDonne);
   ptr_ficDonne=fopen("tempDonne", "r");
 
-  while(mot!=<mot>)   //on passe tout ce qui est avant la liste de mot
-  {
-    fscanf(ptr_ficDonne, "%s", mot);
-  }
 
-  while(mot!=</mot>)
+  while(!feof(ptr_ficDonne))
   {
-    fscanf(ptr_ficDonne, "%s", mot);    //on lit le mot
-    if(mot!=</mot>)
+    fscanf(ptr_ficDonne, "%s ", mot);    //on lit le mot
+    if(strcmp("</mot>", mot)==0)
+    {
+      cestUnMot=0;
+    }
+    if(cestUnMot)
     {
       fscanf(ptr_ficDonne, "%*s");    //on ignore le nombre d'occurences
       nbMots++;   //on incrémente le nombre de mots
@@ -216,6 +216,10 @@ int compareFichierTexte(char * fichierDonne , char * fichierCompare)
       {
         nbCommun++;   //on incrémente le nombre de mots communs entre les 2
       }
+    }
+    if(strcmp("<mot>", mot)==0)
+    {
+      cestUnMot=1;
     }
   }
   pourcentage=(100*nbCommun)/nbMots;    //pourcentage de ressemblance

f266e9c4c676ceaacc22b4ba0f7189c31fb58121
Tara LEMAIRE <mat0lemaire@gmail.com>
2017-01-18 04:46:53 +0100

    Delete indexationSon.h

--- Src/Indexation/indexationSon.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INDEXATIONSON_H
-#define _INDEXATIONSON_H
-#include "Fichier.h"
-#include "pileDescripteur.h"
-
-void genDescripteurSon(t_Fichier fichier, t_PileDescripteur *ptr_PileSon);
-
-#endif

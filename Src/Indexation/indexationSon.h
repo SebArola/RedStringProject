4e7ce72684ff9899ca2b5835bef584c94904bafc
Etienne Delarue <etienne.delarue31@gmail.com>
2017-01-19 20:10:48 +0100

    mieux

--- Src/Indexation/indexationSon.h
+++ Src/Indexation/indexationSon.h
@@ -4,18 +4,18 @@
 #include "Fichier.h"
 #include "pileDescripteur.h"
 
-
-
 typedef struct DescSon{
-  char *address;      /*  adresse dans la base descripteur */
-  int nbWindows;     /*  nombre de fenêtre dans l'histogramme */
-  int ** histogram;   /*  Histogram de taille k*m.
+  char id[10];	      /*  identifiant du descripteur */
+  int nbWindows;      /*  nombre de fenêtre dans l'histogramme */
+  int** histogram;    /*  Histogram de taille k*m.
                             k : nombre fenetre
                             m : nombre intervalles entre -1 et 1*/
 }t_DescSon;
 
+int readStruct(FILE* file, void * wStruct, size_t structSize);
 int fileSize(FILE* file); // renvoie la taille du fichier passer en paramètre
-double endianSwap_double(double dbl); // memcpy d'un double vers un autre en inversant les octets
+double Swap_double(double dbl); // memcpy d'un double vers un autre double
 t_DescSon * CreerDescSon(FILE* file); // créer un t_DescSon ave un fichier binaire
-void genDescripteurSon(t_Fichier fichier, t_PileDescripteur *ptr_PileSon);
+void genDecripteurSon(t_Fichier fichier, t_PileDescripteur *ptr_PileSon);
 #endif
+

4e7e904b4e7dca5aa7a7a90e44ebe6bd08dd651e
Etienne Delarue <etienne.delarue31@gmail.com>
2017-01-26 11:40:46 +0100

    fonctions

--- Src/Indexation/indexationImage.h
+++ Src/Indexation/indexationImage.h
@@ -12,5 +12,7 @@ void genDescripteurImage(t_Fichier fichier, t_PileDescripteur *ptrPileImage);
 void decimalToBinaire(int x, int *res, int bit);
 int binaireToDecimal(int *bin, int bit);
 int quantif(int *binR, int *binV, int *binB, int nbQuantif);
+void imageNB(char ** descripteur, t_Fichier fichier);
+void imageRVB(char ** descripteur, t_Fichier fichier);
 //MODIF
 #endif

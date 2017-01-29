fc7b01a518c674f5984de1b3139c524d3f4b4c2e
Tara LEMAIRE <mat0lemaire@gmail.com>
2017-01-29 19:19:16 +0100

    Update indexationSon.h

--- Src/Indexation/indexationSon.h
+++ Src/Indexation/indexationSon.h
@@ -1,6 +1,10 @@
 #ifndef _INDEXATIONSON_H
 #define _INDEXATIONSON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "Fichier.h"
 #include "pileDescripteur.h"
 
@@ -12,10 +16,18 @@ typedef struct DescSon{
                             m : nombre intervalles entre -1 et 1*/
 }t_DescSon;
 
-int readStruct(FILE* file, void * wStruct, size_t structSize);
-int fileSize(FILE* file); // renvoie la taille du fichier passer en paramètre
-double Swap_double(double dbl); // memcpy d'un double vers un autre double
-t_DescSon * CreerDescSon(FILE* file); // créer un t_DescSon ave un fichier binaire
+/*fileSize renvoie la taille en octet du fichier passé en paramètre*/
+int fileSize(FILE* file);
+
+/*lireDonnees lit le fichier binaire et renvoie -1 si on se trouve à la fin*/
+int lireDonnees(FILE* file, void * typeDonnee, size_t tailleDonnee);
+
+/*echangeDouble échange les octets d'une variable de type double*/
+double echangeDouble(double dbl);
+
+/*Créer un descripteur son de type t_DescSon défini plus haut en binaire*/
+t_DescSon * CreerDescSon(FILE* file); 
+
+/*Génère le descripteur Son en fichier .txt à partir du descripteur de type binaire*/
 void genDecripteurSon(t_Fichier fichier, t_PileDescripteur *ptr_PileSon);
 #endif
-
